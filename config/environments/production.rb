1cd078646dd045571d429f85332e9b23f7ea59a3
smathieu <simon.math@gmail.com>
2011-07-21 16:28:19 -0400

    Host for reset emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,8 @@ BlogReminder::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.default_url_options = { :host => "http://www.activeblogr.com" }
+  
 
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
