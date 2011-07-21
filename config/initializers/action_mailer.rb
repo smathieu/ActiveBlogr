aa7700cb98d341fb2d2082965b8ec1e833f48a77
smathieu <simon.math@gmail.com>
2011-07-21 13:39:25 -0400

    Send grid config.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,9 +1,9 @@
-ActionMailer::Base.smtp_settings = {  
-  :address              => "smtp.gmail.com",  
-  :port                 => 587,  
-  :domain               => "gmail.com",  
-  :user_name            => "simon.math",  
-  :password             => "",  
-  :authentication       => "plain",  
-  :enable_starttls_auto => true  
-}  
+ActionMailer::Base.smtp_settings = {
+  :address        => "smtp.sendgrid.net",
+  :port           => "25",
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => ENV['SENDGRID_DOMAIN']
+}
+
