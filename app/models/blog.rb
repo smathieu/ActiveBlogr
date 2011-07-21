f57c235338d7278a606016780195f7dd647962a3
smathieu <simon.math@gmail.com>
2011-07-21 17:02:52 -0400

    Use base64 instead of url safe basse 64

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -40,7 +40,7 @@ class Blog < ActiveRecord::Base
   private
   def generate_token(column)
     begin
-      self[column] = SecureRandom.urlsafe_base64
+      self[column] = SecureRandom.base64
     end while self.class.exists?(column => self[column])
   end
 end
