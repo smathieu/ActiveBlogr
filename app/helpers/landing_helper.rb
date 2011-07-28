31e7e96e696e9072ec9d6de310fe0f690bb560a4
smathieu <simon.math@gmail.com>
2011-07-28 12:33:15 -0400

    jQuery validation of input.

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -1,6 +1,6 @@
 module LandingHelper
   def frequency_options
-    options_for_select [['I want to blog every...', 0],
+    options_for_select [['I want to blog every...', nil],
       ['Day', 1],
       ['Week', 7],
       ['Month', 30],
--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -1,15 +1,17 @@
 $(function() {
-  $('input.email').focus(function() {
+  $('input.with_default').focus(function() {
     if (this.value == this.defaultValue) {
       this.value = ""
     }
   });
 
-  $('input.email').focusout(function() {
+  $('input.with_default').focusout(function() {
     if (this.value == "") {
       this.value = this.defaultValue;
     }
   });
+
+  $('#new_blog').validate();
 });
 
 
